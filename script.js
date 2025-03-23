c50e782db549ce2a410bd6751e591e266006c65a
martvanesch <martvanesch01@gmail.com>
2025-03-23 19:21:55 +0100

    update portfolio

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 console.log('linked');
-
-let name = document.getElementById('name');
-let articles = document.querySelectorAll('article');
-console.log(articles);
-let viewportHeight = window.innerHeight;
-let hiddenHeight = viewportHeight * 0.2;
-
-function checkArticles() {
-    articles.forEach(article => {
-        console.log(article.getBoundingClientRect().top);
-        console.log(hiddenHeight);
-
-        if (article.getBoundingClientRect().top < hiddenHeight) {
-            article.classList.add('sizeDown');
-            article.style.zIndex = -10;
-        } else {
-            article.classList.remove('sizeDown');
-        }
-    });
-}
-
-document.addEventListener('scroll', function() {
-    requestAnimationFrame(checkArticles);
-});
+setTimeout(function() {
+  document.querySelector('body').classList.add('visible');
+}, 50);
